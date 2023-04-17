804ba6c23184295365b29fde1dfa2d7719300017
Philipp Reber <98315359+PhilippReber@users.noreply.github.com>
2023-04-17 17:35:02 +0200

    Add files via upload

---
+++ send_smooth_data/send_smooth_data.ino
@@ -0,0 +1,20 @@
+int N_SAMPLES_TO_AVERAGE = 300;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+double val = 0;
+void loop() {
+  while (!Serial.available());
+  Serial.readStringUntil('\n');
+
+  for (int i = 0; i < N_SAMPLES_TO_AVERAGE; i++) {  
+    val += analogRead(A0);
+  }
+
+  val /= N_SAMPLES_TO_AVERAGE;
+  Serial.println(val);
+  
+}
