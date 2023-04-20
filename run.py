7d57c464fbd5bd0f1ff1653ff9aead259523699c
Philipp Reber <98315359+PhilippReber@users.noreply.github.com>
2023-04-20 14:39:29 +0200

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ ser_trigger = serial.Serial('COM4')
 freqs = [0.4, 0.45, 0.48, 0.59, 0.67]  # movement speed in Hz (top to bottom)
 demands = [0.092, 0.094, 0.096, 0.098, 0.100]  # of max force (adjusted)
 
-task = Force_Static_Task(n_trials=5, inlet=ser, outlet=ser_trigger, demand=0.05)
-# task = Force_Move_Task(freqs, demands, n_trials=10, inlet=ser, outlet=ser_trigger)
+# task = Force_Static_Task(n_trials=5, inlet=ser, outlet=ser_trigger, demand=0.05)
+task = Force_Move_Task(freqs, demands, n_trials=10, inlet=ser, outlet=ser_trigger)
 
 task.run()
